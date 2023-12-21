3207d4af72e9dfebd86f689f7865524cccad0801
yves848 <yves.godart@gmail.com>
2023-12-21 19:42:02 +0100

    day03 - part 2

--- DAY03/part1.py
+++ DAY03/part1.py
@@ -5,24 +5,69 @@ import re
 
 file = os.path.dirname(__file__)
 
-D = open(os.path.join(file,"Example.txt")).read().strip()
+D = open(os.path.join(file,"Data.txt")).read().strip()
 L = D.split("\n")
-
-gears = ()
+os.system("cls")
+gears = []
 ii = 0
+sum = 0
 for l in L:
-  i = str(l).find('*')
-  print(l)
-  if i > -1:
-    # gear found
-    r=re.findall(r"\d{1,}",l)
-    print(r)
-    if ii > 0:
-      # not first line  
-      r=re.findall(r"\d{1,}",L[ii-1])
-      print(r)
-    if ii < len(L)-1:
-      # not last line
-      r=re.findall(r"\d{1,}",L[ii+1])
-      print(r)
+  i = 0
+  print(f"{ii} - {l}")
+  while i > -1:
+    i = str(l).find('*',i+1) 
+    if i > -1:
+      # Same line
+      gear = []
+      # print(f'Index : {i}')
+      num = ""
+      j = i-1
+      while (j > 0 and  l[j].isdigit()):
+        num = l[j] + num
+        j-= 1
+      if (num.isnumeric()):
+        gear.append(num)
+      num = ""
+      j = i+1
+      while (j < len(l) and l[j].isdigit()):
+        num = num + l[j]
+        j+= 1
+      if (num.isnumeric()):
+        gear.append(num)
+
+      if ii > 0:
+        # Line UP  
+        l2 = L[ii-1] 
+        finds=re.findall(r"\d{1,}",l2)
+        i1 = 0
+        for f in finds:
+          lf = len(f)
+          i1 = str(l2).index(f,i1) 
+          if (i in range(i1-1,i1+lf+1)):
+            gear.append(f)
+          i1 = i1 + lf
+        # print(finds)
+              
+      if ii < len(L)-1:
+        # Line Down
+        l2 = L[ii+1] 
+        finds=re.findall(r"\d{1,}",l2)
+        i1 = 0
+        for f in finds:
+          lf = len(f)
+          i1 = str(l2).index(f,i1) 
+          if (i in range(i1-1,i1+lf+1)):
+            gear.append(f)
+          i1 += lf
+        # print(finds)
+      print(gear)
+
+      if (len(gear) >= 2):
+        gears.append(int(gear[0])*int(gear[1]))
   ii+=1
+
+# print(gears)
+sum = 0
+for gear in gears:
+  sum += int(gear)
+print(sum)
