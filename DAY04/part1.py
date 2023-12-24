33a73e6531e43b804b27507b48396be3fbc970f3
yves848 <yves.godart@gmail.com>
2023-12-24 16:56:29 +0100

    DAY 04 Part 1

--- DAY04/part1.py
+++ DAY04/part1.py
@@ -2,13 +2,30 @@ import os
 
 file = os.path.dirname(__file__)
 
-D = open(os.path.join(file,"Example.txt")).read().strip()
+D = open(os.path.join(file,"Data.txt")).read().strip()
 C = D.split('\n')
 play = {}
+totalpoints = 0
 for l in C:
   Cards = l.split(":")
-  for Card in Cards:
-    winnings = Card.split('|')
+  numbers = Cards[1].split('|')
+  winnings = []
+  for c in (numbers[0].strip().split(' ')):
+    if str(c).strip() != '':
+      winnings.append(c)
+  nums =  []
+  for c in (numbers[1].strip().split(' ')):
+    if str(c).strip() != '':
+      nums.append(c)
+  play[Cards[0]]=(winnings,nums)
+  points = 0
+  for num in nums:
+    if num in winnings:
+      if points == 0:
+        points += 1
+      else:
+        points = points * 2
 
-print(Cards)
-print(winnings)
+  totalpoints += points
+print(play)
+print(totalpoints)
